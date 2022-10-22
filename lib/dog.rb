196216ba6b3f4a540bb1ecec12bf3c4158aebe6b
Timothy <tkkibet13@gmail.com>
2022-10-22 21:02:43 +0300

    Assigniment completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
+    attr_reader :name
+    attr_reader :breed
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed        
+    end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-
+    attr_reader :name
+    attr_reader :job
+    def name=(name)
+        @name = name        
+    end
+    def job=(job)
+        @job = job
+    end
 end
